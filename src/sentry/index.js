496d5c7453cd3c0ce86e6a4fbccb22d025b17b97
pdf <pedrodiasfonseca@gmail.com>
2017-08-09 00:14:47 -0300

    Add SentryBot to manage Sentry webhooks integration

--- src/sentry/index.js
+++ src/sentry/index.js
@@ -1,5 +1,30 @@
+const bot = require('./sentry-slackbot');
+const projectChannels = require('./project-channels');
+
 function sentryController(req, res, next) {
-    console.log(req.body);
+    const data = req.body;
+    const channel = projectChannels[data.project];
+    const message = '';
+    const color = data.level === 'error' ? '#f72600' : '#E8E8E8';
+    const params = {
+        icon_url: 'http://www.emoticonswallpapers.com/avatar/movies/The-Terminator.jpg',
+        attachments: [
+            {
+                title: data.message,
+                title_link: data.url,
+                color,
+                fields: [
+                    { title: 'Project', value: data.project_name, short: true },
+                    { title: 'Culprit', value: data.culprit, short: true }
+                ],
+                footer: 'Sentry',
+                footer_icon: 'http://img4.imagetitan.com/img4/small/16/16_sentry-icon.png',
+                ts: data.event.received
+            }
+        ]
+    };
+
+    bot.postMessageToChannel(channel, message, params);
     res.send(req.body);
 }
 
---
+++ src/sentry/project-channels.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'api': 'danger-api',
+    'bff-affiliate': 'danger-bff-affiliate',
+    'bff-customer': 'danger-bff-customer',
+    'bff-integration': 'danger-bff-integratio',
+    'lembrador': 'danger-lembrador',
+    'website': 'danger-site'
+};
