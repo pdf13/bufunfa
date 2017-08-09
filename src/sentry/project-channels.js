20dae3854113796a8e8d7fdb642123847a127bcf
pdf <pedrodiasfonseca@gmail.com>
2017-08-09 00:25:15 -0300

    Add and remove new channels to SentryBot

--- src/sentry/project-channels.js
+++ src/sentry/project-channels.js
@@ -1,9 +1,9 @@
 module.exports = {
     'admin': 'danger-admin',
     'api': 'danger-api',
-    'bff-affiliate': 'danger-bff-affiliate',
     'bff-customer': 'danger-bff-customer',
     'bff-integration': 'danger-bff-integratio',
     'lembrador': 'danger-lembrador',
-    'website': 'danger-site'
+    'website': 'danger-site',
+    'worker': 'danger-worker'
 };
